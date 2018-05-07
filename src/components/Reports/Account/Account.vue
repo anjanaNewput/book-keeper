04293d8eb175795d6c15dd130848db504bdca4a7
anjanaNewput <anjana@newput.com>
2018-05-07 17:18:55 +0530

    Added ledger account

--- src/components/Reports/Account/Account.vue
+++ src/components/Reports/Account/Account.vue
@@ -1,3 +1,26 @@
 <template src="./account.html"></template>
 <script src="./account.js"></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ledger-acc {
+  width: 80%;
+  margin: 40px auto;
+  padding: 30px 50px;
+  background-color: #fff;
+}
+.wrapper {
+  text-align: left;
+  .display-interval {
+    label, span {
+      font-size: 16px;
+    }
+  }
+  .table {
+    margin-bottom: 0px;
+  }
+}
+@media screen and (max-width: '768px') {
+  .ledger-acc {
+    width: 100%;
+  }
+}
+</style>
--- src/components/Reports/Account/account.js
+++ src/components/Reports/Account/account.js
@@ -1,3 +1,77 @@
+import enteries from '@/assets/json/enteries.json'
+import AppDataTable from '../../Common/AppTable/AppDataTable.vue'
 export default {
-  name: 'Account'
+  name: 'Account',
+  data () {
+    return {
+      rows: [{
+        srno: '',
+        creditAmt: '',
+        date: null,
+        debitAmt: '',
+        particulars: 'Total',
+        balance: '',
+        children: enteries.data
+      }],
+      columns: [
+        {
+          label: 'SNo.',
+          field: 'srno'
+        },
+        {
+          label: 'Date',
+          field: 'date',
+          type: 'date',
+          dateInputFormat: 'DD/MM/YYYY',
+          dateOutputFormat: 'MMM Do YY'
+        },
+        {
+          label: 'Particulars',
+          field: 'particulars',
+          sortable: false
+        },
+        {
+          label: 'DR',
+          field: 'debitAmt',
+          type: 'number',
+          headerField: this.sumDR,
+        },
+        {
+          label: 'CR',
+          field: 'creditAmt',
+          type: 'number',
+          headerField: this.sumCR,
+        },
+        {
+          label: 'Balance',
+          field: 'balance',
+          type: 'number'
+        },
+      ]
+    }
+  },
+  components: {
+    'AppTable':AppDataTable
+  },
+  methods: {
+    sumCR(rowObj) {
+    	let sum = 0;
+      for (let i = 0; i < rowObj.children.length; i++) {
+        sum += rowObj.children[i].creditAmt;
+      }
+      return sum;
+    },
+    sumDR(rowObj) {
+    	let sum = 0;
+      for (let i = 0; i < rowObj.children.length; i++) {
+        sum += rowObj.children[i].debitAmt;
+      }
+      return sum;
+    }
+  },
+  computed: {
+    reportData () {
+      return this.$store.state.reportData
+    }
+  }
 }
